65bfbb593fc486c3baaa46a6d1756d3242f7f711
Alisa Ilinykh <ilinykh.alice@yandex.ru>
2024-09-26 14:05:57 +1000

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,3 @@
+select count(customer_id) as customers_count
+from customers;
+--Запрос считает количество значений в столбце customer_id из таблицы customers. Значение сохраняется с названием customers_count.
